e5e44f366039856d433fb3bb0c563bb146bc08b8
Alisson Lima <alissongabriel010907@gmail.com>
2026-04-24 23:02:12 -0300

    - finaliza integração de funcionalidade de recuperação/redefinição de senha

--- app/EsqueciSenha.tsx
+++ app/EsqueciSenha.tsx
@@ -2,33 +2,107 @@ import AppButton from "@/components/AppButton";
 import AppText from "@/components/AppText";
 import Input from "@/components/input";
 import { Colors } from "@/constants/GlobalStyles";
+import AuthService from "@/service/AuthService";
+import { RedefineSenhaRequest } from "@/service/model/RedefineSenhaRequest";
 import PopupService from "@/utils/PopupService";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
+import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function EsqueciSenha() {
-  const handleRecuperarSenha = () => {
-    PopupService.success(
-      "Instruções de recuperação de senha enviadas para seu email!",
-    );
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [redefinirSenhaData, setRedefinirSenhaData] = useState(
+    {} as RedefineSenhaRequest,
+  );
+  const [isRedefinirSenha, setIsRedefinirSenha] = useState(false);
+
+  const handleRecuperarSenha = async () => {
+    try {
+      await AuthService.recuperarSenha({ email });
+      PopupService.success(
+        "Instruções de recuperação de senha enviadas para seu email!",
+      );
+      setIsRedefinirSenha(true);
+    } catch (error) {
+      PopupService.error("Erro ao tentar recuperar a senha. Tente novamente.");
+    }
+  };
+
+  const handleRedefinirSenha = async () => {
+    try {
+      await AuthService.redefinirSenha(redefinirSenhaData);
+      PopupService.success(
+        "Senha redefinida com sucesso! Voltando para tela de login...",
+      );
+      setTimeout(() => router.replace("/StartPage"), 2000);
+    } catch (error) {
+      PopupService.error("Erro ao tentar redefinir a senha. Tente novamente.");
+    }
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <View style={styles.formContainer}>
-          <AppText style={styles.title}>Esqueceu a senha?</AppText>
-          <Input label="E-mail" placeholder="exemple@mail.com" />
-          <AppButton
-            text="Recuperar senha"
-            backgroundColor={Colors.roxo}
-            onPress={handleRecuperarSenha}
-            style={{ marginTop: 50 }}
-          />
-          <AppText style={styles.backToLogin}>
-            <Link href={"/StartPage"}>Voltar para login</Link>
+          <AppText style={styles.title}>
+            {isRedefinirSenha ? "Redefinição de senha" : "Esqueceu a senha?"}
           </AppText>
+          {!isRedefinirSenha ? (
+            <Input
+              label="E-mail"
+              placeholder="exemple@mail.com"
+              value={email}
+              onChangeText={setEmail}
+            />
+          ) : (
+            <>
+              <Input
+                label="Código"
+                placeholder="Digite o código recebido"
+                value={redefinirSenhaData.codigo}
+                onChangeText={(codigo) =>
+                  setRedefinirSenhaData({ ...redefinirSenhaData, codigo })
+                }
+              />
+              <Input
+                label="Nova senha"
+                placeholder="Digite sua nova senha"
+                value={redefinirSenhaData.novaSenha}
+                onChangeText={(novaSenha) =>
+                  setRedefinirSenhaData({ ...redefinirSenhaData, novaSenha })
+                }
+              />
+            </>
+          )}
+          {!isRedefinirSenha ? (
+            <AppButton
+              text="Recuperar senha"
+              backgroundColor={Colors.roxo}
+              onPress={handleRecuperarSenha}
+              style={{ marginTop: 50 }}
+            />
+          ) : (
+            <AppButton
+              text="Redefinir senha"
+              backgroundColor={Colors.roxo}
+              onPress={handleRedefinirSenha}
+              style={{ marginTop: 50 }}
+            />
+          )}
+          {!isRedefinirSenha ? (
+            <AppText style={styles.backToLogin}>
+              <Link href={"/StartPage"}>Voltar para login</Link>
+            </AppText>
+          ) : (
+            <AppText
+              style={styles.backToLogin}
+              onPress={() => setIsRedefinirSenha(false)}
+            >
+              Voltar
+            </AppText>
+          )}
         </View>
       </View>
     </SafeAreaView>
@@ -40,7 +114,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   formContainer: {
-    width: "80%",
+    width: "90%",
     padding: 20,
     backgroundColor: "#fff",
     borderRadius: 10,
